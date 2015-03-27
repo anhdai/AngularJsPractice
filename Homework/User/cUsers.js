b375b8b5dfbe9cd2f5faa263cb6d27a6d27d548e
anhdai <lamanhdai@vn.oro.com>
2015-03-27 16:29:07 +0700

    improve process

--- Homework/User/cUsers.js
+++ Homework/User/cUsers.js
@@ -1,15 +1,16 @@
 angular.module('cApp', []).controller('UserCtrl', function($scope) {
     $scope.fName = '';
     $scope.lName = '';
+    $scope.id = 0;
     $scope.users = [{id:1, fName:'Tom', lName:'Hilton'},
         {id:2, fName:'Peter', lName:'Hamilton'}];
     $scope.edit = false;
     $scope.addUser = function() {
         if($scope.edit) {
-            $scope.edit = false;
-            $scope.users.splice(id-1,id-1,{id:count,fName:$scope.fName, lName:$scope.lName});
+            $scope.users.splice($scope.id-1,1,{id:$scope.id,fName:$scope.fName, lName:$scope.lName});
             $scope.fName = '';
             $scope.lName = '';
+            $scope.id = 0;
         }
         else {
             count = $scope.users.length + 1;
@@ -17,10 +18,12 @@ angular.module('cApp', []).controller('UserCtrl', function($scope) {
             $scope.fName = '';
             $scope.lName = '';
         }
+        $scope.edit = false;
     };
     $scope.editUser = function(id) {
         $scope.fName = $scope.users[id-1].fName;
         $scope.lName = $scope.users[id-1].lName;
         $scope.edit = true;
+        $scope.id = id;
     }
 });
