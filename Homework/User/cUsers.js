efb2f33bd4d95b561596813a4878adedb42c5bad
anhdai <lamanhdai@vn.oro.com>
2015-04-27 16:48:44 +0700

    modify user function

--- Homework/User/cUsers.js
+++ Homework/User/cUsers.js
@@ -1,29 +1,30 @@
-angular.module('cApp', []).controller('UserCtrl', function($scope) {
-    $scope.fName = '';
-    $scope.lName = '';
-    $scope.id = 0;
-    $scope.users = [{id:1, fName:'Tom', lName:'Hilton'},
-        {id:2, fName:'Peter', lName:'Hamilton'}];
-    $scope.edit = false;
-    $scope.addUser = function() {
-        if($scope.edit) {
-            $scope.users.splice($scope.id-1,1,{id:$scope.id,fName:$scope.fName, lName:$scope.lName});
-            $scope.fName = '';
-            $scope.lName = '';
-            $scope.id = 0;
-        }
-        else {
-            count = $scope.users.length + 1;
-            $scope.users.push({id:count,fName:$scope.fName, lName:$scope.lName});
-            $scope.fName = '';
-            $scope.lName = '';
-        }
-        $scope.edit = false;
-    };
-    $scope.editUser = function(id) {
-        $scope.fName = $scope.users[id-1].fName;
-        $scope.lName = $scope.users[id-1].lName;
-        $scope.edit = true;
-        $scope.id = id;
+angular.module('cApp', []).controller('UserCtrl', function($scope, $http) {
+  $scope.fName = '';
+  $scope.lName = '';
+  $scope.id = 0;
+  $http.get('data.json').success(function(data){
+    $scope.users = data;
+  });
+  $scope.edit = false;
+  $scope.addUser = function() {
+    if($scope.edit) {
+      $scope.users.splice($scope.id-1,1,{id:$scope.id,fName:$scope.fName, lName:$scope.lName});
+      $scope.fName = '';
+      $scope.lName = '';
+      $scope.id = 0;
     }
+    else {
+      count = $scope.users.length + 1;
+      $scope.users.push({id:count,fName:$scope.fName, lName:$scope.lName});
+      $scope.fName = '';
+      $scope.lName = '';
+    }
+    $scope.edit = false;
+  };
+  $scope.editUser = function(id) {
+    $scope.fName = $scope.users[id-1].fName;
+    $scope.lName = $scope.users[id-1].lName;
+    $scope.edit = true;
+    $scope.id = id;
+  }
 });
