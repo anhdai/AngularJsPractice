cab265d6ba4f6a16f9cbf8ffe6864ebf1ff49bae
anhdai <lamanhdai@vn.oro.com>
2015-04-29 11:45:26 +0700

    reference: http://curran.github.io/screencasts/introToAngular/exampleViewer/#/

--- Homework/User/cUsers.js
+++ Homework/User/cUsers.js
@@ -14,13 +14,49 @@ var userApp = angular.module('cApp', ['ngRoute']);
   });
 });
 
-userApp.controller('UserList', function($scope, $http) {
+userApp.factory('getUser', function($http){
+    var cache;
+    function getList(callback){
+        if(cache){
+            callback(cache);
+        } else{
+            $http.post('data.json').success(function(data){
+                cache = data;
+                callback(data);
+            });
+        }
+    }
+    return {
+        list: getList,
+        find: function(Id, callback){
+            $http.post("data.json").success(function(data){
+                userDetail = data.filter(function(entry){
+                    return parseInt(Id) === entry.id;
+                })[0];
+                callback(userDetail);
+            });
+        }
+    }
+});
+
+userApp.directive('user',function(){
+    return{
+        scope:{
+            user: "="
+        },
+        restrict: "A",
+        templateUrl: "userId.html"
+    };
+});
+
+userApp.controller('UserList', function($scope, getUser) {
   $scope.fName = '';
   $scope.lName = '';
   $scope.id = 0;
-  $http.post('data.json').success(function(data){
-    $scope.users = data;
+  getUser.list(function(data){
+      $scope.users = data;
   });
+  
   $scope.edit = false;
   $scope.addUser = function() {
     if($scope.edit) {
@@ -42,16 +78,15 @@ userApp.controller('UserList', function($scope, $http) {
     $scope.lName = $scope.users[id-1].lName;
     $scope.edit = true;
     $scope.id = id;
-  }
+  };
 });
 
-userApp.controller('UserDetail', function($scope, $routeParams, $http) {
-    $scope.anotherId = $routeParams.ID;
-    $http.post("data.json").success(function(data){
-        $scope.userDetail = data.filter(function(entry){
-            console.log(typeof parseInt($scope.anotherId));
-            console.log(typeof entry.id);
-            return parseInt($scope.anotherId) === entry.id;
-        })[0];
+userApp.controller('UserDetail', function($scope, $routeParams, getUser) {
+    getUser.find($routeParams.ID, function(userDetail){
+        $scope.userDetail = userDetail;
     });
+});
+
+userApp.filter('encodeURI', function(){
+    return window.encodeURI;
 });
